cc461374610678858dfcaa53301913a0f0fd8a4d
Mitchell Kristie <mitchellkristie.mk@gmail.com>
2020-01-01 20:12:00 -0500

    tagger is working. First round of data from this tagger version 2

--- semeval_11_2020/feature_engineering/tagger.py
+++ semeval_11_2020/feature_engineering/tagger.py
@@ -3,19 +3,21 @@ from nltk.tokenize import PunktSentenceTokenizer as pkt
 from nltk import pos_tag
 import glob
 import pickle
-import sklearn
-import sklearn_crfsuite
-import fasttext
+
 
 MAIN = '/home/mpk3/Natural_Language_Processing/semeval_11_2020'
 
 # Models
 MODEL_DIR = MAIN + '/models'
+
 # DATA
 DATA_DIR = MAIN + '/provided/datasets'
 SPAN_DIR = DATA_DIR + '/train-labels-task1-span-identification'
 TECH_DIR = DATA_DIR + '/train-labels-task2-technique-classification'
 ARTICLE_DIR = DATA_DIR + '/train-articles'
+
+# Labeled Data
+LAB_DATA = MAIN + '/labeled_data'
 
 
 class Tagger():
@@ -41,6 +43,7 @@ class Tagger():
         # Propaganda Span Dictionary: {'articlename':[(start, fin)]}
         self.span_dict = {}
 
+        # Everything below this is cleared for each article
         # Unadultered article text
         self.raw_text = ''
 
@@ -69,6 +72,9 @@ class Tagger():
         # pos trigrams
         self.pos_trigrams = []
 
+        # class tags: p = propaganda, o = other
+        self.class_tags = []
+
     def clear(self):
         '''Clears all article specific variables
         Does not delete any of the models or the propaganda
@@ -83,6 +89,7 @@ class Tagger():
         self.tokenized_sentences = []
         self.trigrams = []
         self.pos_trigrams = []
+        self.class_tags = []
 
     def build_span_dict(self, span_directory):
         ''' This function reads in all the spans from all of the task1 directory
@@ -105,10 +112,11 @@ class Tagger():
                 spans = list(open(article, "r"))
                 spans = [line.split('\t') for line in spans]
                 name = spans[0][0]
-                self.span_dict[name] = [(int(span[1]), int(span[2])) for span in spans]
+                self.span_dict[name] = [(int(span[1]), int(span[2]))
+                                        for span in spans]
 
-    def pickle_dump(self, object, fout):
-        pickle.dump(object, open(fout, "wb"))
+    def pickle_dump(self, obj, fout):
+        pickle.dump(obj, open(fout, "wb"))
 
     def load_span_dict(self, span_dict_pickle):
         '''This function loads an already created span dictionary
@@ -150,7 +158,7 @@ class Tagger():
 
     def create_sent_lists(self):
         '''
-        DEPRECATED! NOT IN USE!
+        Used for po taggings
 
         Creates an array object where each row is a list of numbers
         corresponding to the tokens in each sentence. This is needed for both
@@ -177,7 +185,7 @@ class Tagger():
 
         self.tokenized_sentences = [[tokenized_sentence]]
         '''
-
+        
         for span in self.sent_spans:
             sentence = []
             sent_range = set(range(span[0], span[1]))
@@ -186,6 +194,32 @@ class Tagger():
                     token = self.raw_text[token_span[0]:token_span[1]]
                     sentence.append(token)
             self.tokenized_sentences.append(sentence)
+
+    def p_o_tag(self):
+        '''Creates p o tagging for propaganda spans.
+        p for propaganda
+        o for other
+        creates: self.class_tags
+        This create_token_list() and create_sent_lists() could all be
+        refactored.
+        '''
+        self.create_sent_lists()
+        # Label character as other
+        po_tags = ['o' for x in range(len(self.raw_text))]
+
+        # Label everything that is propaganda as p
+        if self.article_name in self.span_dict.keys():
+            for prop_span in self.span_dict[self.article_name]:
+                for index in range(prop_span[0], prop_span[1]):
+                    po_tags[index] = 'p'
+
+        # Create unflat class tags
+        for sent_list in self.unflat_sent:
+            po_tag_sent = []
+            for token_index in sent_list:
+                span_start = self.token_spans[token_index][0]
+                po_tag_sent.append(po_tags[span_start])
+            self.class_tags.append(po_tag_sent)
 
     def pos_tag(self):
         '''Tags tokens using indices from token_spans and sent_spans
@@ -227,46 +261,54 @@ class Tagger():
         '''Same as trigram but for POS tags'''
         self.pos_trigrams = [self.generate_trigram(pos_tag_sentence)
                              for pos_tag_sentence in self.tags]
-       
+
 
 """Driver"""
 
-def create_feature_map(tagger_object):
-  feat_map = {}
-  feat_map[tagger_object]
-   
-   
-        self.token_spans = []
-        self.sent_spans = []
-        self.unflat_sent = []
-        self.pos_tags = []                                                                                                                                                                    
-        self.trigrams = []
 
-extractor = Tagger()
-filter.build_span_dict(SPAN_DIR)
-span_dictionary = filter.span_dict
-filter.pickle_dump(span_dictionary, 'span_dictionary')
-
-filter = Tagger()
-filter.load_span_dict('span_dictionary')
-
-files = glob.glob(ARTICLE_DIR, '/*.labels')
-
-for file in files:
-  filter.load_article(file)
-  filter.create_token_spans()
-  filter.create_sent_spans()
-  filter.create_sent_lists() 
-  filter.create_token_list()
-  filter.pos_tag()
-  filter.trigram()
-  filter.pos_trigram()
-  feature_json = filter.create_feature_json()
-  filter.pickle_dump
+def create_feature_lists(fe):
+    sentence_feat_list = []
+    i = 0
+    for sent in fe.unflat_sent:
+        feature_sentence = []
+        j = 0
+        for token in sent:
+            feature_map = {'pos': fe.tags[i][j],
+                           'token': fe.tokenized_sentences[i][j],
+                           'prev_tok': fe.trigrams[i][j][0],
+                           'next_tok': fe.trigrams[i][j][1],
+                           'prev_pos': fe.pos_trigrams[i][j][0],
+                           'next_pos': fe.pos_trigrams[i][j][1],
+                           'class': fe.class_tags[i][j]}
+            feature_sentence.append(feature_map)
+            j = j + 1
+        sentence_feat_list.append(feature_sentence)
+        i = i + 1
+    return sentence_feat_list
 
 
+out_str = MODEL_DIR + '/span_dict.pickle'
+e = Tagger()
+e.load_span_dict(out_str)
+files = glob.glob(ARTICLE_DIR + '/*.txt')
 
-
+trial = '/trial1'
+for article in files:
+    try:
+        e.load_article(article)
+        e.create_sent_spans()
+        e.create_token_spans()
+        e.create_token_list()
+        e.pos_tag()
+        e.create_token_trigrams()
+        e.create_pos_trigrams()
+        e.p_o_tag()
+        features = create_feature_lists(e)
+        fout = LAB_DATA + trial + '/' + e.article_name + '_f.pickle'
+        e.pickle_dump(features, fout)
+        e.clear()
+    except Exception:
+        print(article)
 
 # Tests
 # Span dict tests
@@ -287,12 +329,12 @@ assert test_array == test_extractor.span_dict['999001970'], 'Load Span Dict 2'
 
 # Load article tests
 article = '/home/mpk3/Natural_Language_Processing/semeval_11_2020'\
-    + '/provided/datasets/train-articles/article701225819.txt'
+    + '/provided/datasets/train-articles/article736757214.txt'
 extractor = Tagger()
 extractor.load_span_dict(out_str)
 extractor.load_article(article)
-assert extractor.article_name == '701225819', 'Load article 1'
-assert len(extractor.raw_text) == 6254
+assert extractor.article_name == '736757214', 'Load article 1'
+#assert len(extractor.raw_text) == 6254
 
 # Tagger tests
 extractor = Tagger()
@@ -315,3 +357,24 @@ assert extractor.generate_trigram(test) ==\
     test_result, 'Trigram 1'
 extractor.create_token_trigrams()
 extractor.create_pos_trigrams()
+
+# Class tag test
+extractor.p_o_tag()
+assert  len(extractor.class_tags[0]) ==\
+    len(extractor.tokenized_sentences[0]), 'PO 1'
+
+trial = '/trial1'
+e = Tagger()
+e.load_span_dict(out_str)
+e.load_article(article)
+e.create_sent_spans()
+e.create_token_spans()
+e.create_token_list()
+e.pos_tag()
+e.create_token_trigrams()
+e.create_pos_trigrams()
+e.p_o_tag()
+features = create_feature_lists(e)
+fout = LAB_DATA + trial + '/' + e.article_name + '_f.pickle'
+e.pickle_dump(features, fout)
+        

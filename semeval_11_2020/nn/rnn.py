b7502a98246f1232c27e1eec82739f0799fbe6ce
Mitchell Kristie <mitchellkristie.mk@gmail.com>
2020-02-08 00:00:42 -0500

    Revamped the padding to how I originally had it. Now I need to train and fit the model again. I also want to start introducing embeddings and some manually extracted features like title information

--- semeval_11_2020/nn/rnn.py
+++ semeval_11_2020/nn/rnn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import pickle
 from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 DATA_DIR = '/home/mpk3/Natural_Language_Processing/' +\
     'semeval_11_2020/nn/data/clean_data.pickle'
@@ -19,17 +20,25 @@ def preprocess_data(d):
             tokens.append(token[0])
             i_o.append(token[1])
             vocab_set.add(token[0])
+
+        tokens[:0] = ['<start>']
+        tokens.append('<end>')
+        i_o[:0] = ['<start>']
+        i_o.append('<end>')
+
         tokenized_sentences.append(tokens)
         labels.append(i_o)
+
+    vocab_set.add('<start>')
+    vocab_set.add('<end>')
+    label_set.add('<start>')
+    label_set.add('<end>')
+
     return tokenized_sentences, labels, vocab_set, label_set
 
 
-def encode(vocab_set, tokenized_sentences, label_set, labels):
-    '''Encodes tokenized sentence matrix into integers and then
-    pads them to a length of 100
-    This also originally padded sentences with keras api but I am
-    switching over to using tensorflows Dataset class to get more
-    familiar with the built in preprocessing classes/methods in TF'''
+def pad_encode(vocab_set, tokenized_sentences, label_set, labels):
+    '''Encodes and pads sentences and labels'''
     encoder = tfds.features.text.TokenTextEncoder(vocab_set)
     label_encoder = tfds.features.text.TokenTextEncoder(label_set)
 
@@ -39,56 +48,43 @@ def encode(vocab_set, tokenized_sentences, label_set, labels):
     encoded_labels = [label_encoder.encode(' '.join(sent))
                       for sent in labels]
 
-    everything = tuple(zip(encoded_sentences, encoded_labels))
-    return everything
+    padded_sentences = tf.keras.preprocessing.\
+        sequence.pad_sequences(encoded_sentences,
+                               padding='post',
+                               maxlen=100)
+    padded_labels = tf.keras.preprocessing.\
+        sequence.pad_sequences(encoded_labels,
+                               padding='post',
+                               maxlen=100)
+
+    return padded_sentences, padded_labels
 
 # Loading Data
 data = pickle.load(open(DATA_DIR, 'rb'))
 tok_sent, labels, vocab_set, label_set = preprocess_data(data)
 
-# Encoding
-encoder = tfds.features.text.TokenTextEncoder(vocab_set)
-label_encoder = tfds.features.text.TokenTextEncoder(label_set)
+input_tensor, target_tensor = pad_encode(vocab_set, tok_sent,
+                                         label_set, labels)
 
-encoded_sentences = [encoder.encode(' '.join(sent))
-                     for sent in tok_sent]
-encoded_labels = [label_encoder.encode(' '.join(sent))
-                  for sent in labels]
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val =\
+    train_test_split(input_tensor, target_tensor, test_size=0.2)
 
 
-# W/O Padding
-# r_s = tf.ragged.constant(encoded_sentences)
-# r_l = tf.ragged.constant(encoded_labels)
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 64
+steps_per_epoch = len(input_tensor_train)//BATCH_SIZE
+embedding_dim = 256
+units = 1024
+vocab_inp_size = len(vocab_set)+1
+vocab_tar_size = len(label_set)+1
 
-# dataset = tf.data.Dataset.from_tensors((r_s, r_l))
-# dataset = dataset.padded_batch(100)
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
 
-# Padding
-train_padded_sentences = tf.keras.preprocessing.\
-    sequence.pad_sequences(encoded_sentences[0:13000],
-                           padding='post',
-                           maxlen=100)
-train_padded_labels = tf.keras.preprocessing.\
-    sequence.pad_sequences(encoded_labels[0:13000],
-                           padding='post',
-                           maxlen=100)
 
-test_padded_sentences = tf.keras.preprocessing.\
-    sequence.pad_sequences(encoded_sentences[13000:],
-                           padding='post',
-                           maxlen=100)
-test_padded_labels = tf.keras.preprocessing.\
-    sequence.pad_sequences(encoded_labels[13000:],
-                           padding='post',
-                           maxlen=100)
 
-train_data = tf.data.Dataset.from_tensors((train_padded_sentences,
-                                           train_padded_labels))
-test_data = tf.data.Dataset.from_tensors((test_padded_sentences,
-                                          test_padded_labels))
 
-train_batches = train_data
-test_batches = test_data
+
 
 model = tf.keras.Sequential([
   layers.Embedding(encoder.vocab_size, 16),

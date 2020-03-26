0147a5238c28a62bf8a941e6c0538332d6cda2ae
Mitchell Kristie <mitchellkristie.mk@gmail.com>
2020-03-26 18:01:38 -0400

    Traditional and Simplified Chinese works now. Now I need to build a NN to deal with translation. It will probably be a bit inaccurate but at least it will be a good reason to mess around with TF

--- Utilities/cli_parser/parse.py
+++ Utilities/cli_parser/parse.py
@@ -7,66 +7,53 @@ import re
 # import gensim.downloader as api
 
 
-# Command Line Arguments
+##### Command Line Arguments #####
 parser = argparse.ArgumentParser()
+# Parse
 parser.add_argument("-c", "--cli",
                     help='Command line string parser;\
                     \'quit\' to exit; \'display\' for dependency graph',
                     action="store_true")
+# Displacy Dependency Graph
 parser.add_argument("-d", "--display",
                     help='Display dependency graph; \'C-c C-c\' to exit',
                     action="store_true")
+# Gensim Similarity
 parser.add_argument("-s", "--similarity",
                     help='Gensim similarity',
                     action="store_true")
 args = parser.parse_args()
 parser = argparse.ArgumentParser()
 
+##### Models #####
 # Spacy Initialization
 nlp = spacy.load("en_core_web_sm")
 doc = nlp('No sentence was loaded')
-
 # Gensim Initialization
 word_vectors = pickle.load(open('wv.pickle', 'rb'))
-
 # word_vectors = api.load("glove-wiki-gigaword-100")
 # pickle.dump(word_vectors, open('wv.pickle', 'wb'))
 
 
+##### Mandarin #####
 def mandarin_parse(sentence):
     ce_dict = pickle.load(open('ce_dict.pickle', 'rb'))
     p = Pinyin()
     words = jieba.cut(sentence)
-    for word in words:
+    for word in words:   # May need to be edited for aesthetics
+        definitions = [(definition['english'], definition['pinyin'])
+                       for definition in ce_dict
+                       if definition['simplified'] == word
+                       or definition['traditional'] == word]
         print(word,
               p.get_pinyin(word, splitter=' ',
-                           tone_marks='numbers'))
+                           tone_marks='numbers'),
+              definitions)  # May need to be edited for aesthetics
     return
 
 
-def similarity(word):
-    """
-    Gensim similarity using built in function for quick
-    similarity lookup. Can take a list of words or a 
-    single word.
-
-    Parameters:
-    -----------
-
-    word: []/string
-    """
-    # try:
-    #    if type(eval(word)) == list:
-    #        result = word_vectors.most_similar(positive=word)
-    #except NameError as ne:
-        
-    #else:
-    # result = word_vectors.most_similar(positive=[word])
-
-    return # result
-
-
-if args.cli:
+##### Driver/Script #####
+if args.cli:  # Parse
     while True:
         sentence = input()
         if re.search("[\u4e00-\u9FFF]", sentence):
@@ -88,7 +75,7 @@ if args.cli:
                         token.pos_, token.tag_,
                         token.dep_, token.shape_,
                         token.is_stop))
-elif args.display:
+elif args.display:  # Displacy
     sentence = input()
     if re.search("[\u4e00-\u9FFF]", sentence):
             mandarin_parse(sentence)
@@ -98,18 +85,17 @@ elif args.display:
         doc = nlp(sentence)
         spacy.displacy.serve(doc, style="dep", page=True)
 
-elif args.similarity:
+elif args.similarity:  # Gensim
     while True:
         sentence = input()
-        if sentence == 'quit':
+        if sentence in ('quit', 'q'):
             break
         else:
-            result = word_vectors.most_similar(positive=[sentence])
-            print(result)
-# if sentence == 'quit' or 'q':
-        #     break
-        # else:
-        #     print(similarity(sentence))
+            try:
+                result = word_vectors.most_similar(positive=[sentence])
+                print(result)
+            except KeyError as ke:
+                print('Not in Gensim Vocab\n Spacy provides subword vectors')
 
 else:
-    print('Requires option flag\nPython parse.py --help for options ')
+    print('Requires option flag\nPython parse.py --help for options')

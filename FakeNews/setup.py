b508f3695d3b80570774b2635f9fe1de69698848
Mitchell Kristie <mitchellkristie.mk@gmail.com>
2020-04-20 13:43:11 -0400

    Initial Commit

---
+++ FakeNews/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='FakeNews',
+    version='0.1dev',
+    packages=['main'],
+    license='Apache License, Version 2.0',
+    long_description=open('README.txt').read()
+)
